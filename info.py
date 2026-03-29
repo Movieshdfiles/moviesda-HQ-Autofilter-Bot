e8cefb3dd8bfec3a517789bf2a73702074fe3c5b
Movieshdfiles <sridharloganthan71@gmail.com>
2026-03-29 17:17:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -123,7 +123,7 @@ THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "900"))
 # Channel & Group Links Configuration
 # ============================
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/Moviesda_search_group_HQ') # Group link for the bot
-OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Moviesda_CEO'') # Owner link for the bot
+OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Moviesda_CEO') # Owner link for the bot
 UPDATE_CHNL_LNK = environ.get('UPDATE_CHNL_LNK', 'https://t.me/+xUur10DNB243MTc1') # Update channel link for the bot
 
 # ============================
