9b6e2683867f189a1d885050d7d49856941de8f8
Movieshdfiles <sridharloganthan71@gmail.com>
2026-08-12 19:45:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -107,14 +107,14 @@ TUTORIAL_2 = environ.get("TUTORIAL_2", "https://t.me/how_to_download_moviesda/77
 TUTORIAL_3 = environ.get("TUTORIAL_3", "https://t.me/how_to_download_moviesda/77")   # Third tutorial link for verification
 
 # Verification (Must Fill All Veriables. Else You Got Error
-SHORTENER_API = environ.get("SHORTENER_API", "64cd0169cc09ecc7fcf48d5725b85faa8bced0de") # Shortener API key
-SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", "liteshort.com") # Shortener website
+SHORTENER_API = environ.get("SHORTENER_API", "dbe5bbe132ad94487e2f0cb614b78239c74beffa") # Shortener API key
+SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", "modishort.com") # Shortener website
 
-SHORTENER_API2 = environ.get("SHORTENER_API2", "64cd0169cc09ecc7fcf48d5725b85faa8bced0de")  # Shortener API key for second website
-SHORTENER_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", "liteshort.com") # Shortener website for second website
+SHORTENER_API2 = environ.get("SHORTENER_API2", "dbe5bbe132ad94487e2f0cb614b78239c74beffa")  # Shortener API key for second website
+SHORTENER_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", "modishort.com") # Shortener website for second website
 
-SHORTENER_API3 = environ.get("SHORTENER_API3", "64cd0169cc09ecc7fcf48d5725b85faa8bced0de")  
-SHORTENER_WEBSITE3 = environ.get("SHORTENER_WEBSITE3", "liteshort.com") # Shortener website for third website
+SHORTENER_API3 = environ.get("SHORTENER_API3", "dbe5bbe132ad94487e2f0cb614b78239c74beffa")  
+SHORTENER_WEBSITE3 = environ.get("SHORTENER_WEBSITE3", "modishort.com") # Shortener website for third website
 
 TWO_VERIFY_GAP = int(environ.get('TWO_VERIFY_GAP', "600")) # Time gap for two-step verification in seconds (default: 20 minutes)
 THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "900"))    
